fa85f4c94f22482d265cd74df7549255272a8f63
edg4ras - Edgar Malinauskas <edg4ras@gmail.com>
2014-12-15 21:44:16 +0200

    quick fix vienetų pridėjimas

--- edit_clasificator.php
+++ edit_clasificator.php
@@ -7,20 +7,36 @@ $username = $_SESSION['username'];
 $administrator = isAdministrator($username);
 if (loggedIn($where) && isAdministrator($username)) {
 	if (!empty($_POST)) {
-		
-	};
+		$id = $_POST["id"];
+		$name = $_POST["name"];
+
+   		$queryEdit = "UPDATE quantities SET name='$name' WHERE id='$id'";
+   		mysql_query($queryEdit);
+
+		echo "Pakeitimai išsaugoti.";
+	}	
+
+	if (!empty($_GET['edit'])) {
+		$editingID = $_GET['edit'];
+
+	} elseif (!empty($_GET['delete'])) {
+		$id = $_GET['delete'];
+
+   		$queryDelete = "DELETE FROM quantities WHERE id=$id";
+   		mysql_query($queryDelete);
+	}
 
 	echo "<h2>Matavimo vienetai</h2>";
 
 	$queryGetQuantities = "SELECT * FROM quantities";
 	$quantities_result = mysql_query($queryGetQuantities); 
 	
-	echo "<table style=\"width:20%; text-align: center;\">
+	echo "<table style=\"width:30%; text-align: center;\">
 		  	<tr>
 		    	<th>Kurėjas</th>
 		    	<th>Klasifikatorius</th> 
 		    	<th>Panaudota</th>
-		    	<th>Veiksmas</th>
+		    	<th></th>
 	  	  	</tr>";
 
 	while($quantity = mysql_fetch_row($quantities_result)) {
@@ -34,12 +50,28 @@ if (loggedIn($where) && isAdministrator($username)) {
    				$panaudota_kartu += 1;
    			}
 
-   			echo "<tr>
-			    	<td>$quantity[1]</td>
-			    	<td>$quantity[2]</td> 
-			    	<td>$panaudota_kartu</td>
-			    	<td></td>
-			 	  </tr>";
+   			if ($panaudota_kartu == 0) {
+   				$controls = "<a href=\"edit_clasificator.php?edit=$quantity[0]\">Redaguoti</a>
+   							 <a href=\"edit_clasificator.php?delete=$quantity[0]\">Ištrinti</a>";
+   			}
+
+   			if (!($quantity[0] == $editingID)) {
+	   			echo "<tr>
+					 	<td>$quantity[1]</td>
+				    	<td>$quantity[2]</td> 
+				    	<td>$panaudota_kartu</td>
+				    	<td>$controls</td>
+					 </tr>";
+			} else {
+	   			echo "<form action=\"edit_clasificator.php\" method=\"post\">
+	   				 	<tr>
+						 	<td>$quantity[1]</td>
+					    	<td><input type=\"text\" name=\"name\" value=\"$quantity[2]\"></td> 
+					    	<td>$panaudota_kartu</td>
+					    	<td><input type=\"hidden\" name=\"id\" value=\"$quantity[0]\"><input type=\"submit\" value=\"Išsaugoti\"></td>
+					 	</tr>
+					 </form>";
+			}
        	}
    	}
 
