b45f31fb3b76d4e6e04f55bcfc278410ac4e9416
edg4ras - Edgar Malinauskas <edg4ras@gmail.com>
2014-12-18 15:03:23 +0200

    matavimo vienetu input patikra

--- vnt_klasifikatorius.php
+++ vnt_klasifikatorius.php
@@ -11,35 +11,52 @@ if (loggedIn($where)) {
 	
 	if (!empty($_POST)) {
 		$newQuantityName = $_POST['quantity'];
-		$used = false;
 
-		// ar toks jau yra sukurtas?
-		$queryQuantities = "SELECT name FROM quantities";
+		// Validation
+			// Wrong format
+			$minQuantityLength = 2;
+			$maxQuantityLength = 20;
 
-		$quantities_result = mysql_query($queryQuantities);
-
-		while($quantity = mysql_fetch_row($quantities_result)) {
-			if ($quantity[0] == $newQuantityName) {
-				$used = true;
-				break;
+			if ( (strlen($newQuantityName) < $minQuantityLength) or (strlen($newQuantityName) > $maxQuantityLength) ) {
+				$wrongFormat = true;
+			} else {
+				$wrongFormat = false;
 			}
-		}
 
-		if ($used) {
-			echo "Toks \"$newQuantityName\" vienetas jau egzistuoja.";
-		} else {
-			$sql = "INSERT INTO quantities (username, name) VALUES ('$username', '$newQuantityName')";
+			// Duplicate
+			$duplicate = false;
 
-			if (mysql_query($sql)) {
-				echo "Sėkmingai papildyta \"$newQuantityName\" vienetu.";
+			$queryQuantities = "SELECT name FROM quantities";
+
+			$quantities_result = mysql_query($queryQuantities);
+
+			while($quantity = mysql_fetch_row($quantities_result)) {
+				if ($quantity[0] == $newQuantityName) {
+					$duplicate = true;
+					break;
+				}
 			}
-		}
-		echo "<br /><br /><a href=\"vnt_klasifikatorius.php\">Įvesti kitą</a>";
+
+			if ($duplicate) {
+				echo "Vienetas su vardu \"$newQuantityName\" jau egzistuoja.";
+			} else if ($wrongFormat) {
+				echo "Leidžiamas vieneto pavadinimo dydis yra nuo $minQuantityLength simbolių iki $maxQuantityLength";
+			} else {
+				$sql = "INSERT INTO quantities (username, name) VALUES ('$username', '$newQuantityName')";
+
+				if (mysql_query($sql)) {
+					echo "Sėkmingai papildyta \"$newQuantityName\" vienetu.";
+				}
+			}
+
+			echo 	"<br />
+				  	 <br />
+				  	 <a href=\"vnt_klasifikatorius.php\">Įvesti kitą</a>";
 	} else {
-			echo '<form action="vnt_klasifikatorius.php" method="post">
+		echo 	'<form action="vnt_klasifikatorius.php" method="post">
 					Pavadinimas: <input type="text" name="quantity">
 					<input type="submit" value="Papildyti">
-				  </form>';
+				 </form>';
 	}
 
 	echo "<h2>Matavimo vienetai</h2>";
