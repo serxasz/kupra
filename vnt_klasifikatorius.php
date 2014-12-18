cc266c6f674110769d72523d0f146a08771e0aee
edg4ras - Edgar Malinauskas <edg4ras@gmail.com>
2014-12-18 16:21:19 +0200

    vnt klasif. puslapiavimas

--- vnt_klasifikatorius.php
+++ vnt_klasifikatorius.php
@@ -60,8 +60,24 @@ if (loggedIn($where)) {
 	}
 
 	echo "<h2>Matavimo vienetai</h2>";
+	// Pagination 
+		// Total number of rows in table
+			$query = "SELECT COUNT(*) as num FROM quantities";
+			$total_pages = mysql_fetch_array(mysql_query($query));
+			$total_pages = $total_pages[num];
+			echo "Viso įrašų: $total_pages";
 
-	$queryQuantities = "SELECT * FROM quantities";
+		/* Setup vars for query. */
+			$targetpage = "vnt_klasifikatorius.php"; 	//your file name  (the name of this file)
+			$limit = 5; 									//how many items to show per page
+			$page = $_GET['page'];
+
+			if ($page) 
+				$start = ($page - 1) * $limit; 			//first item to display on this page
+			else
+				$start = 0;								//if no page var is given, set start to 0
+
+	$queryQuantities = "SELECT * FROM quantities LIMIT $start, $limit";
 	$quantities_result = mysql_query($queryQuantities);
 
 	echo "<table style=\"width:20%; text-align: center;\">
@@ -75,6 +91,98 @@ if (loggedIn($where)) {
        		  </tr>";
    	}
 	echo "</table>";
+
+	/* Setup page vars for display. */
+	if ($page == 0) $page = 1;					//if no page var is given, default to 1.
+	$prev = $page - 1;							//previous page is page - 1
+	$next = $page + 1;							//next page is page + 1
+	$lastpage = ceil($total_pages/$limit);		//lastpage is = total pages / items per page, rounded up.
+	$lpm1 = $lastpage - 1;						//last page minus 1
+	/* 
+		Now we apply our rules and draw the pagination object. 
+		We're actually saving the code to a variable in case we want to draw it more than once.
+	*/
+	$pagination = "";
+
+	if($lastpage > 1)
+	{	
+		$pagination .= "<div class=\"pagination\">";
+		//previous button
+		if ($page > 1) 
+			$pagination.= "<a href=\"$targetpage?page=$prev\"> <<< </a>";
+		else
+			$pagination.= "<span class=\"disabled\"> <<< </span>";	
+		
+		//pages	
+		if ($lastpage < 7 + ($adjacents * 2))	//not enough pages to bother breaking it up
+		{	
+			for ($counter = 1; $counter <= $lastpage; $counter++)
+			{
+				if ($counter == $page)
+					$pagination.= "<span class=\"current\">$counter</span>";
+				else
+					$pagination.= "<a href=\"$targetpage?page=$counter\">$counter</a>";					
+			}
+		}
+		elseif($lastpage > 5 + ($adjacents * 2))	//enough pages to hide some
+		{
+			//close to beginning; only hide later pages
+			if($page < 1 + ($adjacents * 2))		
+			{
+				for ($counter = 1; $counter < 4 + ($adjacents * 2); $counter++)
+				{
+					if ($counter == $page)
+						$pagination.= "<span class=\"current\">$counter</span>";
+					else
+						$pagination.= "<a href=\"$targetpage?page=$counter\">$counter</a>";					
+				}
+				$pagination.= "...";
+				$pagination.= "<a href=\"$targetpage?page=$lpm1\">$lpm1</a>";
+				$pagination.= "<a href=\"$targetpage?page=$lastpage\">$lastpage</a>";		
+			}
+			//in middle; hide some front and some back
+			elseif($lastpage - ($adjacents * 2) > $page && $page > ($adjacents * 2))
+			{
+				$pagination.= "<a href=\"$targetpage?page=1\">1</a>";
+				$pagination.= "<a href=\"$targetpage?page=2\">2</a>";
+				$pagination.= "...";
+				for ($counter = $page - $adjacents; $counter <= $page + $adjacents; $counter++)
+				{
+					if ($counter == $page)
+						$pagination.= "<span class=\"current\">$counter</span>";
+					else
+						$pagination.= "<a href=\"$targetpage?page=$counter\">$counter</a>";					
+				}
+				$pagination.= "...";
+				$pagination.= "<a href=\"$targetpage?page=$lpm1\">$lpm1</a>";
+				$pagination.= "<a href=\"$targetpage?page=$lastpage\">$lastpage</a>";		
+			}
+			//close to end; only hide early pages
+			else
+			{
+				$pagination.= "<a href=\"$targetpage?page=1\">1</a>";
+				$pagination.= "<a href=\"$targetpage?page=2\">2</a>";
+				$pagination.= "...";
+				for ($counter = $lastpage - (2 + ($adjacents * 2)); $counter <= $lastpage; $counter++)
+				{
+					if ($counter == $page)
+						$pagination.= "<span class=\"current\">$counter</span>";
+					else
+						$pagination.= "<a href=\"$targetpage?page=$counter\">$counter</a>";					
+				}
+			}
+		}
+		
+		//next button
+		if ($page < $counter - 1) 
+			$pagination.= "<a href=\"$targetpage?page=$next\"> >>> </a>";
+		else
+			$pagination.= "<span class=\"disabled\"> >>> </span>";
+		$pagination.= "</div>\n";		
+	}
+
+	echo $pagination;
+
 
    	echo "<br /><br /><a href=\"index.php\">Atgal</a>";
 
