f83a0a6c4c3c5a8a45d83965c6bf13de591f9824
serxasz <haris865@inbox.lt>
2015-01-04 19:54:29 +0200

    titulinis psl

--- index.php
+++ index.php
@@ -6,18 +6,36 @@ include($_SESSION['lang']);
 $where=$phrase[81]; 
 if (loggedIn($where)) {
     
-    echo'Geriausiai įvertinti receptai:</h4><br>
+    echo'<h4>Naujausi receptai:</h4><br>
     <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
   <!-- Wrapper for slides -->
-  <div class="carousel-inner" role="listbox">
-    <div class="item active">
-      <img src="https://lh5.ggpht.com/ddv92tewQBxVzMJlWdaq8msObi6E00h0FOxuaMmnTQxmWek9HtDjBJx1ZfP6l6vYfCAO=w300" alt="patiekalas">
-    </div>
-    <div class="item">
-      <img src="https://sveikagyvensena.files.wordpress.com/2009/11/makaronai3.jpg" alt="makaronai">
-    </div>
-  </div>
-
+  <div class="carousel-inner" role="listbox">';
+    $active = true;
+    $sql = "SELECT * FROM recipes ORDER BY id ASC LIMIT 50";
+	$result = mysql_query($sql);
+	while($row = mysql_fetch_assoc($result)){
+        if ($active) {
+            echo'<div class="item active">';
+            $active = false;
+        } else {
+            echo'<div class="item">';
+        }
+        $file = glob("uploads/recipes/".$row['id']."/*.{jpg,jpeg,png,gif}",GLOB_BRACE);
+        if (!empty($file)) {
+        foreach ($file as $i) {
+            if ($i == "uploads/recipes/".$row['id']."/1.jpg" or $i == "uploads/recipes/".$row['id']."/1.jpeg" or $i == "uploads/recipes/".$row['id']."/1.png" or $i == "uploads/recipes/".$row['id']."/1.gif") {
+                        $image = '<img src="'.$i.'" style="height:400px; width:400px;" alt="photo">';
+                    }
+                }
+        } else {
+            $image = '<img src="images/no-photo.jpg" alt="photo" style="height:400px; width:400px;">';
+        }
+		echo'<a href="visi_receptai.php?view='.$row['id'].'">'.$image.'</a>';
+        echo'<a href="visi_receptai.php?view='.$row['id'].'"><b>'.$row['name'].'</b></a>';
+        echo'</div>';
+	}
+    
+    echo'</div>
   <!-- Controls -->
   <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
     <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
