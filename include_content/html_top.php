385cb84907a3d90661ce957733117e085f06d11b
edg4ras - Edgar Malinauskas <edg4ras@gmail.com>
2015-01-04 18:47:41 +0200

    rcp pridejimas update

--- include_content/html_top.php
+++ include_content/html_top.php
@@ -84,8 +84,20 @@
     echo'   <script>
             function productAddition(str, id, rcpID) {
                 if (str == "") {
-                    document.getElementById("addProduct").innerHTML = "";
-                    return;
+                    if (window.XMLHttpRequest) {
+                        // code for IE7+, Firefox, Chrome, Opera, Safari
+                        xmlhttp = new XMLHttpRequest();
+                    } else {
+                        // code for IE6, IE5
+                        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+                    }
+                    xmlhttp.onreadystatechange = function() {
+                        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+                            document.getElementById("addProduct").innerHTML = xmlhttp.responseText;
+                        }
+                    }
+                    xmlhttp.open("GET","produktai2.php?add=empty"+"&id="+id+"&rcpid="+rcpID,true);
+                    xmlhttp.send();
                 } else { 
                     if (window.XMLHttpRequest) {
                         // code for IE7+, Firefox, Chrome, Opera, Safari
--- produktai2.php
+++ produktai2.php
@@ -6,59 +6,65 @@ include('config.php');
     $rcpID = intval($_GET['rcpid']);
 
     if ($add > 0) {
-		$sql = "INSERT INTO recipe_products (product_amount, product_id, recipe_id) VALUES ('$add', '$productID', '$rcpID')";
+    	// check if we need to add or update
+    	$sql = "SELECT * FROM recipe_products WHERE product_id='$productID' AND recipe_id='$rcpID'";
+    	$queryResult = mysql_query($sql);
 
-		if (mysql_query($sql)) {
-			// get list of products
-		    $viewID = $rcpID;
-		   	$queryProducts = "SELECT * FROM recipe_products WHERE recipe_id='$viewID'";
-			$products = mysql_query($queryProducts);
-
-			if(mysql_num_rows($products) > 0) { 
-				echo "<table style=\"width:20%; text-align: center;\">
-					  	<tr>
-					  		<th></th>
-					  		<th>Produktas</th>
-					    	<th>Vienetas</th>
-					    	<th>Kiekis</th>
-				  	  	</tr>";
-			}
-
-			while ($product = mysql_fetch_row($products)) {
-				$queryProductInfo = "SELECT * FROM products WHERE id='$product[2]'";
-				$result_productInfo = mysql_query($queryProductInfo);
-				$productInfo = mysql_fetch_row($result_productInfo);
-
-				$queryQuantityInfo = "SELECT name FROM quantities WHERE id='$productInfo[2]'";
-				$result_quantityInfo = mysql_query($queryQuantityInfo);
-				$quantityInfo = mysql_fetch_row($result_quantityInfo);		
-		        
-		        $file = glob("uploads/products/*.{jpg,jpeg,png,gif}",GLOB_BRACE);
-		        $image = "";
-		        foreach ($file as $i) {
-		            if ($i == "uploads/products/" . $productInfo[3] . ".jpg" 
-		            	or 
-		            	$i == "uploads/products/" . $productInfo[3] . ".jpeg" 
-		            	or $i == "uploads/products/" . $productInfo[3] . ".png" 
-		            	or $i == "uploads/products/" . $productInfo[3] . ".gif") {
-		                        $image = '<img src="'.$i.'" alt="photo" height="75" width="75">';
-		                    }
-		                }
-
-					    echo 	"<tr>
-					    			<td>$image</td>
-					       			<td>$productInfo[3]</td>
-					       			<td>$quantityInfo[0]</td>
-					       			<td>$product[3]</td>
-					       		</tr>";
-			}
-
-			if(mysql_num_rows($products) > 0) { 
-				echo "</table>";
-			}
-		} else {
-			print_r(mysql_error());
-			echo "!!! FAILLLLL !!!!";
-		}
+    	if (mysql_num_rows($queryResult) == 0) { // Add
+    		$sql = "INSERT INTO recipe_products (product_amount, product_id, recipe_id) VALUES ('$add', '$productID', '$rcpID')";
+			mysql_query($sql);
+    	} else { // Update
+    		$sql = "UPDATE recipe_products SET product_amount='$add', product_id='$productID', recipe_id='$rcpID' WHERE product_id='$productID' AND recipe_id='$rcpID'";
+    		mysql_query($sql);
+    		print_r(mysql_error());
+    	}
     }
+
+	// get list of products
+    $viewID = $rcpID;
+   	$queryProducts = "SELECT * FROM recipe_products WHERE recipe_id='$viewID'";
+	$products = mysql_query($queryProducts);
+
+	if(mysql_num_rows($products) > 0) { 
+		echo "<table style=\"width:20%; text-align: center;\">
+			  	<tr>
+			  		<th></th>
+			  		<th>Produktas</th>
+			    	<th>Vienetas</th>
+			    	<th>Kiekis</th>
+		  	  	</tr>";
+	}
+
+	while ($product = mysql_fetch_row($products)) {
+		$queryProductInfo = "SELECT * FROM products WHERE id='$product[2]'";
+		$result_productInfo = mysql_query($queryProductInfo);
+		$productInfo = mysql_fetch_row($result_productInfo);
+
+		$queryQuantityInfo = "SELECT name FROM quantities WHERE id='$productInfo[2]'";
+		$result_quantityInfo = mysql_query($queryQuantityInfo);
+		$quantityInfo = mysql_fetch_row($result_quantityInfo);		
+        
+        $file = glob("uploads/products/*.{jpg,jpeg,png,gif}",GLOB_BRACE);
+        $image = "";
+        foreach ($file as $i) {
+            if ($i == "uploads/products/" . $productInfo[3] . ".jpg" 
+            	or 
+            	$i == "uploads/products/" . $productInfo[3] . ".jpeg" 
+            	or $i == "uploads/products/" . $productInfo[3] . ".png" 
+            	or $i == "uploads/products/" . $productInfo[3] . ".gif") {
+                        $image = '<img src="'.$i.'" alt="photo" height="75" width="75">';
+                    }
+                }
+
+			    echo 	"<tr>
+			    			<td>$image</td>
+			       			<td>$productInfo[3]</td>
+			       			<td>$quantityInfo[0]</td>
+			       			<td>$product[3]</td>
+			       		</tr>";
+	}
+
+	if(mysql_num_rows($products) > 0) { 
+		echo "</table>";
+	}
 ?>
